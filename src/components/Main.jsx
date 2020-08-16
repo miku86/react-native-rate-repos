191f08dba027572df66896a6f6913fa858e2bf97
miku86 <miku86coding@gmail.com>
2020-08-16 19:31:39 +0200

    Update layout of main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,22 @@
-import Constants from "expo-constants";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import AppBar from "./AppBar";
 import RepositoryList from "./RepositoryList";
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: Constants.statusBarHeight,
     flexGrow: 1,
     flexShrink: 1,
+    display: "flex",
+    flexDirection: "column",
+    width: "100%"
   },
 });
 
 const Main = () => {
   return (
     <View style={styles.container}>
+      <AppBar />
       <Text>Rate Repository App</Text>
       <RepositoryList />
     </View>
