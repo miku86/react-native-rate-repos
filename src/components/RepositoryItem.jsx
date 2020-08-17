c030c54b427aedbf2cdc2f892191c3a8f761c50a
miku86 <miku86coding@gmail.com>
2020-08-17 20:16:19 +0200

    Add image

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,8 +1,17 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+
+const styles = StyleSheet.create({
+  itemImage: {
+    width: 70,
+    height: 70,
+    borderRadius: 4
+  },
+});
 
 const RepositoryItem = ({
   item: {
+    ownerAvatarUrl,
     fullName,
     description,
     language,
@@ -14,6 +23,7 @@ const RepositoryItem = ({
 }) => {
   return (
     <View>
+      <Image style={styles.itemImage} source={{ uri: ownerAvatarUrl }} />
       <Text>Full Name: {fullName}</Text>
       <Text>Description: {description}</Text>
       <Text>Language: {language}</Text>
