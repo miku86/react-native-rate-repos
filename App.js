1789d27871c26b2a661c22a60c99c19934e92e19
miku86 <miku86coding@gmail.com>
2020-08-18 17:09:06 +0200

    Remove unnecessary View

--- App.js
+++ App.js
@@ -1,23 +1,11 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
 import { NativeRouter } from "react-router-native";
 import Main from "./src/components/Main";
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 const App = () => {
   return (
     <NativeRouter>
-      <View style={styles.container}>
-        <Main />
-      </View>
+      <Main />
     </NativeRouter>
   );
 };
